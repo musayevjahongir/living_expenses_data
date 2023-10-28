a719ac52e73bb97bd1948cd3e8102727917de367
musayevjahongir <musayevjahongir5051@gmail.com>
2023-10-28 14:49:36 +0500

    arzon maxsulot

--- least_expensive.py
+++ least_expensive.py
@@ -10,9 +10,13 @@ def least_expensive(file_path: str) -> str:
     Returns:
         str: least expensive item
     """
-    pass
-
-# test
+    import json
+    s=[]
+    f=open(file_path).read()
+    f=json.loads(f)
+    for i in f:
+        s.append(f[i])
+    return min(s)
 file_path = "data.json"
 least_expensive_item = least_expensive(file_path)
 print(least_expensive_item)
